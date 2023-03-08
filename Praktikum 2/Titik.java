a295836b40b71a3c22085aa9a8ae7a2d0327d75b
HanerdID <88916212+HanerdID@users.noreply.github.com>
2023-03-08 15:58:46 +0700

    Update Titik.java

--- Praktikum 2/Titik.java
+++ Praktikum 2/Titik.java
@@ -1,3 +1,12 @@
+/**
+Nama File	: Titik.java
+Penulis		: Fikri Prasetya Nurhidayat
+NIM		: 24060121140153
+Tanggal		: 1 Maret 2023
+Deskripsi	: Kelas yang berisi program utama yang memanfaatkan kelas MTitik
+Lab		: B1
+**/
+
 class Titik {
 	private Double absis;
 	private Double ordinat;
