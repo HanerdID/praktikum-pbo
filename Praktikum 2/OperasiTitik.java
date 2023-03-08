7d83b82300597f4dee75980ffe0a241dd82bdefe
HanerdID <88916212+HanerdID@users.noreply.github.com>
2023-03-08 15:59:21 +0700

    Update OperasiTitik.java

--- Praktikum 2/OperasiTitik.java
+++ Praktikum 2/OperasiTitik.java
@@ -1,3 +1,12 @@
+/**
+Nama File	: OperasiTitik.java
+Penulis		: Fikri Prasetya Nurhidayat
+NIM		: 24060121140153
+Tanggal		: 1 Maret 2023
+Deskripsi	: Kelas yang berisi program utama yang memanfaatkan kelas OperasiTitik
+Lab		: B1
+**/
+
 class OperasiTitik {
 	public void refleksiSumbuX(Titik titik) {
 		Double ordinat = titik.getOrdinat();
