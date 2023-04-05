9976a8ba06fabe497b03bc810dc6950ba0d35724
HanerdID <88916212+HanerdID@users.noreply.github.com>
2023-04-05 21:29:18 +0700

    Update AngkaSial.java

--- Praktikum 6/AngkaSial.java
+++ Praktikum 6/AngkaSial.java
@@ -1,18 +1,16 @@
 /**
-* File		    : AngkaSial.java 01-04-2023
+* File		: AngkaSial.java 01-04-2023
 * Nama / NIM	: Fikri Prasetya Nurhidayat / 24060121140153
 * Deskripsi     : Program penggunaan exception buatan sendiri. Pengenalan klausa 'throw' dan 'throws'
 **/
 
 public class AngkaSial{
-	
 	public void cobaAngka(int angka) throws AngkaSialException{
 		if(angka == 13){
 			throw new AngkaSialException();
 		}
 		System.out.println(angka+" bukan angka sial");
 	}
-	
 	public static void main(String[] args){
 		AngkaSial as = new AngkaSial();
 		try{
@@ -20,7 +18,6 @@ public class AngkaSial{
 			as.cobaAngka(13);
 			as.cobaAngka(12);
 		}catch(AngkaSialException ase){
-			//method getMessage() telah ada pada kelas "Exception"
 			System.out.println(ase.getMessage());
 			System.out.println("hati-hati memasukkan angka!!!");
 		}
