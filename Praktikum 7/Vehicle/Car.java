05dd518ea29f0fa8786f7b0bbb712ce1fedec284
HanerdID <88916212+HanerdID@users.noreply.github.com>
2023-05-09 20:47:15 +0700

    Update Car.java

--- Praktikum 7/Vehicle/Car.java
+++ Praktikum 7/Vehicle/Car.java
@@ -1,5 +1,5 @@
 /**
-* File		    : Vehicle.java 09-05-2023
+* File		    : Car.java 09-05-2023
 * Nama / NIM	: Fikri Prasetya Nurhidayat / 24060121140153
 * Deskripsi     : Car.java adalah adalah kelas yang berisi atribut dan metode dari sub kelas Car
 **/
